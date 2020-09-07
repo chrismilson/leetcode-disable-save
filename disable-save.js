2ac7eafe01ac05f47082b6d09f46f02238510d0c
Chris Milson <chris.w.milson@gmail.com>
2020-09-07 11:37:29 +0900

    Initial Commit

---
+++ disable-save.js
@@ -0,0 +1,12 @@
+var handler = (e) => {
+  if (
+    e.keyCode === 83 &&
+    (e.getModifierState('Control') || e.getModifierState('Meta'))
+  ) {
+    console.log('Cancelled Save.')
+    e.preventDefault()
+    e.stopPropagation()
+  }
+}
+
+document.addEventListener('keydown', handler)
